e71ccaf3c8ef016f28c43049d754cfacf224ca48
Kleidson Almeida <146737840+kleidsonadesign@users.noreply.github.com>
2025-10-18 08:18:30 -0300

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -60,14 +60,6 @@ export function Header() {
               whileHover={{ scale: 1.05 }}
               className="inline-block text-3xl tracking-wider"
             >
-<motion.img
-  src="/images/raio.png" 
-  alt="Raio"
-  // A ÚNICA MUDANÇA É AQUI
-  className="h-6 w-auto mr-2" 
-  initial={{ opacity: 0, scale: 0.5 }}
-  animate={{ opacity: 1, scale: 1 }}
-  transition={{ delay: 0.3, duration: 0.6 }}
 />
 
               
